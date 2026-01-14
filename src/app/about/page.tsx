48eaf07fdaa5e190f256284366e9e551d3ef85b7
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-14 12:16:54 +0600

    feat(loading, error): handle error and loading

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,7 @@
-export default function About() {
+export default async function About() {
+	await new Promise(resolve => setTimeout(resolve, 4000));
+
+	throw new Error("Something went wrong");
 	return (
 		<div>
 			<h1>This is about page component</h1>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/layout/navbar";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -26,8 +27,15 @@ export default function RootLayout({
 	return (
 		<html lang='en' suppressHydrationWarning>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Navbar />
-				{children}
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
