63a94899e375e4263ebe19e782ffc25f06689da7
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-18 19:05:54 +0600

    google login added

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 export default async function RootLayout() {
+	const session = authClient.getSession();
+	console.log(session);
 	return (
 		<div>
 			<Button variant='outline'>Click Here</Button>
--- src/components/modules/authentication/login-form.tsx
+++ src/components/modules/authentication/login-form.tsx
@@ -12,8 +12,12 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"div">) 
 	const handleGoogleLogin = async () => {
 		const data = await authClient.signIn.social({
 			provider: "google",
+			callbackURL: "http://localhost:3000",
 		});
 	};
+
+	const session = authClient.useSession();
+	console.log(session);
 
 	return (
 		<div className={cn("flex flex-col gap-6", className)} {...props}>
