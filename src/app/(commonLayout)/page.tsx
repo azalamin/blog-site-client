6da9a623f9b159360ac60b7f8004f6bee9fc172d
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-20 03:19:09 +0600

    feat(login): signin with google

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function RootLayout() {
-	const session = authClient.getSession();
+	const cookiesStore = await cookies();
+	// console.log(cookiesStore.toString());
+	const res = await fetch("http://localhost:4000/api/auth/get-session", {
+		headers: {
+			Cookie: cookiesStore.toString(),
+		},
+		cache: "no-store",
+	});
+	const session = await res.json();
 	console.log(session);
 	return (
 		<div>
