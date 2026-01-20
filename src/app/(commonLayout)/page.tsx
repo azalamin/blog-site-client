6fda5737e597a7c029ff78a1c17dc5743026a9d5
Al Amin Sheikh <azalamin@MacBookPro.netis.cc>
2026-01-20 15:05:30 +0600

    feat(getSession, userService): done

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,17 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { cookies } from "next/headers";
+import { userService } from "@/service/user.service";
 
-export default async function RootLayout() {
-	const cookiesStore = await cookies();
-	// console.log(cookiesStore.toString());
-	const res = await fetch("http://localhost:4000/api/auth/get-session", {
-		headers: {
-			Cookie: cookiesStore.toString(),
-		},
-		cache: "no-store",
-	});
-	const session = await res.json();
-	console.log(session);
+export default async function HomePage() {
+	const { data } = await userService.getSession();
+
+	console.log(data);
 	return (
 		<div>
 			<Button variant='outline'>Click Here</Button>
---
+++ src/service/user.service.ts
@@ -0,0 +1,25 @@
+import { cookies } from "next/headers";
+
+export const userService = {
+	getSession: async function () {
+		try {
+			const cookiesStore = await cookies();
+			const res = await fetch("http://localhost:4000/api/auth/get-session", {
+				headers: {
+					Cookie: cookiesStore.toString(),
+				},
+				cache: "no-store",
+			});
+			const session = await res.json();
+
+			if (session === null) {
+				return { data: null, error: { message: "Session is missing!" } };
+			}
+
+			return { data: session, error: null };
+		} catch (err) {
+			console.error(err);
+			return { data: null, error: { message: "Something went wrong!" } };
+		}
+	},
+};
